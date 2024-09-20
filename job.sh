efbc7b9d0e0dcbf6ec741e69a344986e539467c7
Step1st <stolomanovas.71@gmail.com>
2024-09-20 17:33:54 +0300

    Added a job to setup env and job to calculate

--- job.sh
+++ job.sh
@@ -2,6 +2,5 @@
 #SBATCH -p main
 #SBATCH -N1
 #SBATCH -n4
-cd ./nmm
-source ./.venv/bin/activate
-python sierpinski_triangle_hpc.py -o test1.csv
+source .venv/bin/activate
+python sierpinski_triangle_hpc.py -o points.csv
